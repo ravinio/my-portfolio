a2a7b7b489ffd1fe8db9b7d5001b0ec32cc288c6
Ravin Io <rio@onestaffmedical.com>
2023-07-21 10:03:11 -0500

    im a goober, kept scroll, gone now

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App: React.FC<AppProps> = ({ themes, onThemeSwitch, activeTheme }) => {
     height:'100%',
     backgroundImage: `url(${theme.images[activeTheme]})`,
     backgroundSize: 'cover',
-    backgroundAttachment: 'scroll',
+    backgroundAttachment: 'fixed',
     backgroundRepeat: 'no-repeat',
     backgroundPosition: theme.styles[activeTheme].backgroundPosition,
     fontFamily: theme.styles[activeTheme].body,
